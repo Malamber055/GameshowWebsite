55eda6dfe784f794bc41978e1eba080638fdae4b
Alex Blizzard <alexblizzard2@gmail.com>
2023-12-26 14:07:07 -0500

    Continued RATW S1 Page w/ css, scripts, and html changes

---
+++ scripts/first-place-finishes.js
@@ -0,0 +1,29 @@
+// Get all rows in the leaderboard table
+const rows = document.querySelectorAll('.leaderboard-table tbody tr');
+
+// Loop through each row
+rows.forEach(row => {
+    // Loop through each cell in the row
+    row.querySelectorAll('td').forEach((cell, index, cells) => {
+        // Get the cell content
+        const content = cell.textContent.trim();
+
+        // Check if it's "1st"
+        if (content === '1st') {
+            // Add a class for styling to the current cell
+            cell.classList.add('first');
+        }
+
+        // Check if it's the final column and content is "2nd"
+        if (index === cells.length - 1 && content === '2nd') {
+            // Add a class for styling to the current cell
+            cell.classList.add('second');
+        }
+
+        // Check if it's the final column and content is "3rd"
+        if (index === cells.length - 1 && content === '3rd') {
+            // Add a class for styling to the current cell
+            cell.classList.add('third');
+        }
+    });
+});
